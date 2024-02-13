96bdc8d8d5c5c1a4e93e5822dfb4ffc9fd9574f6
Tobdhen <114719327+nedbot1@users.noreply.github.com>
2024-02-13 11:16:45 +0600

    wrote my first while loop code

--- loop.js
+++ loop.js
@@ -1,6 +1,15 @@
-for (i = 5; i > 0; i--) {
-  console.log("hello world", i);
-}
-for (i = 0; i <= 5; i++) {
-  console.log("hello world", i);
+//for loop
+// for (i = 5; i > 0; i--) {
+//   console.log("hello world", i);
+// }
+// for (i = 0; i <= 5; i++) {
+//   console.log("hello world", i);
+// }
+
+//while loop
+let i = 1;
+
+while (i <= 5) {
+  console.log(i);
+  i++;
 }
