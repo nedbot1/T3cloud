d6f98a13fbe73f9d3ccb7cc01754d678d1e557b5
Tobdhen <114719327+nedbot1@users.noreply.github.com>
2024-02-13 11:53:04 +0600

    simple multiplication using while loop

--- loop.js
+++ loop.js
@@ -7,9 +7,33 @@
 // }
 
 //while loop
-let i = 1;
+// let i = 1;
 
-while (i <= 5) {
-  console.log(i);
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+// const readline = require("readline-sync");
+
+// let answer = "";
+// while (answer === "yes") {
+//   answer = readline.question("Do you want to continue? (yes/no): ");
+//   if (answer !== "yes" && answer !== "no") {
+//     console.log("Invalid input. Please enter 'yes' or 'no'.");
+//   }
+// }
+
+// console.log("You chose not to continue!");
+
+const readline = require("readline-sync");
+
+let num = Number(
+  readline.question(
+    "Enter a number to generate its multiplication table till 12: "
+  )
+);
+let i = 1;
+while (i <= 12) {
+  console.log(num + " x " + i + " = " + num * i);
   i++;
 }
