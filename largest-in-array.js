4e1065f1a9321a6848ca66994fb4a63fcf934c23
Tobdhen <114719327+nedbot1@users.noreply.github.com>
2024-02-13 14:55:51 +0600

    wrote a simple code to find out the largest and lowest number in array

--- largest-in-array.js
+++ largest-in-array.js
@@ -1,1 +1,15 @@
 const numbers = [12, 44, 9.8473, 30, -12, 57382, 1, 0.3422];
+
+var largestNumber = numbers[0]; // Initialize largest to the first element of the array
+var lowestNumber = numbers[0];
+for (var i = 0; i < numbers.length; i++) {
+  if (numbers[i] > largestNumber) {
+    largestNumber = numbers[i];
+  }
+  if (numbers[i] < lowestNumber) {
+    lowestNumber = numbers[i];
+  }
+}
+
+console.log("The largest number in the array is:", +largestNumber);
+console.log("The lowest number in the array is:", +lowestNumber);
